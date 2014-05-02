e9e1ab6d31af5ddcce538b1a8a98dd55ccab3558
Jennifer Blight <jgblight@gmail.com>
2014-05-02 08:48:56 -0700

    signup form

--- intrepid/intrepid/urls.py
+++ intrepid/intrepid/urls.py
@@ -17,4 +17,5 @@ urlpatterns = patterns('',
 
 urlpatterns += patterns('intrepid_app.views',
 	url(r'^$','index_view'),
+	url(r'^index$','index_view'),
 	url(r'^signup$','signup_view'))
--- intrepid/intrepid_app/views.py
+++ intrepid/intrepid_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
@@ -6,38 +7,67 @@ from django.contrib.auth.decorators import login_required
 from intrepid_app.models import Profile
 from django import forms
 
-from crispy_forms.layout import Submit
 
 # Create your views here.
 
-def login_view(request):
-    message = ''
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST) # A form bound to the POST data
-        print form.errors
-        if form.is_valid(): # All validation rules pass
-            username = form.username
-            password = form.password
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    redirect('index')
-                else:
-                    message = 'User not active'
-            else:
-                message = 'Username or password incorrect'
-        else:
-            message = 'invalid form'
-    form = AuthenticationForm() # An unbound form
+class SignupForm(forms.Form):
+    first_name = forms.CharField(label="First Name", max_length=100)
+    last_name = forms.CharField(label="Last Name", max_length=100)
+    email = forms.EmailField(label="Email")
+    password1 = forms.CharField(label="Password",
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Re-type Password",
+        widget=forms.PasswordInput)
 
-    return render(request, 'login.html', {
-        'form': form,
-        'message': message
-    })
+    error_messages = {
+        'duplicate_email': "Email already registered",
+        'password_mismatch': "The two password fields didn't match",
+        'invalid_email': "Not a valid email address"
+    }
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise forms.ValidationError(
+                self.error_messages['invalid_email'],
+                code='invalid_email',
+            )
+
+        if User.objects.filter(username=email).count():
+            raise forms.ValidationError(
+                self.error_messages['duplicate_email'],
+                code='duplicate_email',
+            )
+
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return self.cleaned_data
 
 def signup_view(request):
-    pass
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+
+            # create user and authenticate
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password2']
+            user = User.objects.create_user(email, email, password)
+            user.save()
+            user = authenticate(username=email,password=password)
+            print 'logging in'
+            login(request, user)
+            print 'redirecting'
+            return redirect(index_view)
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {
+        'form': form
+        })
 
 @login_required
 def index_view(request):
