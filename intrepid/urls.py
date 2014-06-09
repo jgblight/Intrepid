2b135a258a559355c33bf697bc75cf3e305ccfff
Jennifer Blight <jgblight@gmail.com>
2014-06-09 12:24:10 -0400

    fixed api POST

--- intrepid/urls.py
+++ intrepid/urls.py
@@ -4,30 +4,9 @@ admin.autodiscover()
 import django.contrib.auth
 from django.conf import settings
 from django.conf.urls.static import static
-from piston.resource import Resource
-from piston.authentication import HttpBasicAuthentication
-
-from intrepid_app.api import ActiveTripHandler,PinHandler
 
 from django.conf import settings
 from django.http import HttpResponseRedirect
-
-def secure_required(view_func):
-    """Decorator makes sure URL is accessed over https."""
-    def _wrapped_view_func(request, *args, **kwargs):
-        if not request.is_secure():
-            if getattr(settings, 'HTTPS_SUPPORT', True):
-                request_url = request.build_absolute_uri(request.get_full_path())
-                secure_url = request_url.replace('http://', 'https://')
-                return HttpResponseRedirect(secure_url)
-        return view_func(request, *args, **kwargs)
-    return _wrapped_view_func
-
-auth = HttpBasicAuthentication(realm="Intrepid")
-ad = { 'authentication': auth }
-
-active_trip_resource = secure_required(Resource(handler=ActiveTripHandler,**ad))
-new_pin_resource = secure_required(Resource(handler=PinHandler,**ad))
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -50,9 +29,9 @@ urlpatterns += patterns('intrepid_app.views',
     url(r'^file_upload$','file_upload_view'),
     url(r'^pin/(?P<pin_id>[0-9]+)/delete$','delete_pin_view'))
 
-urlpatterns += patterns('',
-    url(r'^api/(?P<username>[0-9A-Za-z_@\+\-\.]+)/trips/active',active_trip_resource),
-    url(r'^api/pin/new',new_pin_resource))
+urlpatterns += patterns('intrepid_app.api',
+    url(r'^api/(?P<username>[0-9A-Za-z_@\+\-\.]+)/trips/active',"active_trips"),
+    url(r'^api/pin/new','new_pin_api'))
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- intrepid_app/api.py
+++ intrepid_app/api.py
@@ -2,51 +2,77 @@ from piston.handler import BaseHandler
 from piston.utils import rc, throttle
 from intrepid_app.models import Trip,User,Pin,Location
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.core import serializers
+from django.conf import settings
 from dateutil.parser import parse
 import datetime
+import json
 
 import sys
 
-class ActiveTripHandler(BaseHandler):
-    allowed_methods = ('GET',)
-    fields = ('id','name')
-    model = Trip
 
-    def read(self, request, username):
-        active_trips = Trip.objects.filter(user__username=username).filter(active=True)
-        return active_trips
+def http_basic_auth(func):
+    def _wrapped_view_func(request, *args, **kwargs):
+        from django.contrib.auth import authenticate, login
+        if not request.is_secure():
+            if getattr(settings, 'HTTPS_SUPPORT', True):
+                request_url = request.build_absolute_uri(request.get_full_path())
+                secure_url = request_url.replace('http://', 'https://')
+                return HttpResponseRedirect(secure_url)
+        if request.META.has_key('HTTP_AUTHORIZATION'):
+            authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
+            if authmeth.lower() == 'basic':
+                auth = auth.strip().decode('base64')
+                username, password = auth.split(':', 1)
+                user = authenticate(username=username, password=password)
+                if user:
+                    login(request, user)
+                    return func(request, *args, **kwargs)
 
-class PinHandler(BaseHandler):
-    allowed_methods = ('GET',)
-    model = Pin
+        response = HttpResponse()
+        response.status_code = 401
+        response['WWW-Authenticate'] = 'Basic realm="Intrepid"'
+        return response
+    return _wrapped_view_func
 
-    #should be 'create' need to fix fucky csrf stuff later
-    def read(self,request): 
+@http_basic_auth
+def active_trips(request,username):
+    trips = Trip.objects.filter(user__username=username).filter(active=True).values("id","name")
+    return HttpResponse(json.dumps(list(trips)), mimetype='application/json')
+
+@csrf_exempt
+@http_basic_auth
+def new_pin_api(request): 
+    if request.method == "POST":
         try:
-            try:
-                trip = Trip.objects.get(pk=int(request.GET.get('trip')));
-            except:
-                return { 'success' : False, 'error' : 'Invalid Trip ID' }
-            
-            try:
-                lat = float(request.GET.get('lat').decode('ascii','ignore'))
-                lon = float(request.GET.get('lon').decode('ascii','ignore'))
-                loc_name = request.GET.get('loc_name')
-                location = Location(lat=lat,lon=lon,name=loc_name)
-                location.save()
-            except:
-                return { 'success' : False, 'error' : 'Problem with location' }
-
-            name = request.GET.get('name')
-            pin_date = parse(request.GET.get('date'))
-            tracks = (request.GET.get('tracks') == "true")
-            text = request.GET.get('text')     
-            try:
-                pin = Pin(trip=trip,name=name,pin_date=pin_date,location=location,tracks=tracks,text=text)
-                pin.save()
-                return { 'success' : True }
-            except:
-                return { 'success' : False, 'error' : 'Could not create pin' }
+            trip = Trip.objects.get(pk=int(request.GET.get('trip')));
         except:
-            print sys.exc_info()
+            response = { 'success' : False, 'error' : 'Invalid Trip ID' }
+        
+        try:
+            lat = float(request.GET.get('lat').decode('ascii','ignore'))
+            lon = float(request.GET.get('lon').decode('ascii','ignore'))
+            loc_name = request.GET.get('loc_name')
+            location = Location(lat=lat,lon=lon,name=loc_name)
+            location.save()
+        except:
+            response = { 'success' : False, 'error' : 'Problem with location' }
+
+        name = request.GET.get('name')
+        pin_date = parse(request.GET.get('date'))
+        tracks = (request.GET.get('tracks') == "true")
+        text = request.GET.get('text')     
+        try:
+            pin = Pin(trip=trip,name=name,pin_date=pin_date,location=location,tracks=tracks,text=text)
+            pin.save()
+            response = { 'success' : True }
+        except:
+            response = { 'success' : False, 'error' : 'Could not create pin' }
+    else:
+        response = { 'success' : False, 'error' : 'Use POST' }
+
+    return HttpResponse(json.dumps(response), mimetype='application/json')
+
+
 
