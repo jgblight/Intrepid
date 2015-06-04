2b4e60f5d1b69834cdd1a3cc6127ed027e96f190
Jennifer Blight <jgblight@gmail.com>
2015-06-03 22:15:06 -0700

    pin view

--- intrepid_app/views.py
+++ intrepid_app/views.py
@@ -5,7 +5,7 @@ import urllib
 import hmac
 import uuid
 from hashlib import sha1
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -15,6 +15,7 @@ from django.conf import settings
 import forms
 import json
 
+
 @login_required
 def index_view(request):
     if request.user.is_authenticated():
@@ -22,11 +23,12 @@ def index_view(request):
     else:
         return redirect('/signup')
 
-def trip_view(request,trip_id):
+
+def trip_view(request, trip_id):
     trip = get_object_or_404(Trip, pk=trip_id)
-    content = { 
-                'trip' : trip,
-                'edit' : trip.user.username == request.user.username }
+    content = {
+        'trip': trip,
+        'edit': trip.user.username == request.user.username}
     if request.GET.has_key('post'):
         if request.GET['post'] == 'last':
             content['post'] = trip.pin_set.count() - 1
@@ -34,11 +36,13 @@ def trip_view(request,trip_id):
             content['post'] = request.GET['post']
     return render(request, 'trip.html', content)
 
-def profile_view(request,username):
+
+def profile_view(request, username):
     return render(request, 'profile.html', {
-        'profile_user' : User.objects.get(username=username),
-        'edit' : username == request.user.username
+        'profile_user': User.objects.get(username=username),
+        'edit': username == request.user.username
     })
+
 
 def signup_view(request):
     if request.method == "POST":
@@ -51,19 +55,20 @@ def signup_view(request):
             password = form.cleaned_data['password2']
             user = User.objects.create_user(username, email, password)
             user.save()
-            user = authenticate(username=username,password=password)
+            user = authenticate(username=username, password=password)
             login(request, user)
             return redirect(index_view)
     else:
         form = forms.SignupForm()
     return render(request, 'signup.html', {
         'form': form
-        })
+    })
+
 
 @login_required
 def new_trip_view(request):
     if request.method == "POST":
-        form = forms.TripForm(request.POST,request.FILES)
+        form = forms.TripForm(request.POST, request.FILES)
         if form.is_valid():
             name = form.cleaned_data['name']
             description = form.cleaned_data['description']
@@ -73,96 +78,122 @@ def new_trip_view(request):
             else:
                 image_file = None
 
-            x = form.cleaned_data['image_x'] if form.cleaned_data['image_x'] else 0
-            y = form.cleaned_data['image_y'] if form.cleaned_data['image_y'] else 0
-            image_width = form.cleaned_data['image_width'] if form.cleaned_data['image_width'] else 1
+            x = form.cleaned_data[
+                'image_x'] if form.cleaned_data['image_x'] else 0
+            y = form.cleaned_data[
+                'image_y'] if form.cleaned_data['image_y'] else 0
+            image_width = form.cleaned_data[
+                'image_width'] if form.cleaned_data['image_width'] else 1
 
-            new_trip = Trip(name=name,text=description,user=request.user,image_file=image_file,image_x=x,image_y=y,image_width=image_width)
+            new_trip = Trip(name=name, text=description, user=request.user,
+                            image_file=image_file, image_x=x, image_y=y, image_width=image_width)
             new_trip.save()
-            return redirect('/trip/'+str(new_trip.id)+'/post')
+            return redirect('/trip/' + str(new_trip.id) + '/post')
     else:
         form = forms.TripForm()
     return render(request, 'new_trip.html', {
         'form': form
-        })
+    })
+
 
 @login_required
-def new_post_view(request,trip_id):
+def new_post_view(request, trip_id):
     trip = get_object_or_404(Trip, pk=trip_id)
     if request.user != trip.user:
-        return redirect(request.META.get("HTTP_REFERRER","/"))
+        return redirect(request.META.get("HTTP_REFERRER", "/"))
     if request.method == "POST":
         form = forms.NewPostForm(request.POST)
         if form.is_valid():
             lat = form.cleaned_data['lat']
             lon = form.cleaned_data['lon']
-            location = Location.objects.create(lat=lat,lon=lon)
+            location = Location.objects.create(lat=lat, lon=lon)
 
             name = form.cleaned_data['name']
             pin_date = form.cleaned_data['date']
             tracks = form.cleaned_data['tracks']
             text = form.cleaned_data['description']
-            pin = Pin.objects.create(trip=trip,name=name,pin_date=pin_date,location=location,tracks=tracks,text=text)
+            pin = Pin.objects.create(
+                trip=trip, name=name, pin_date=pin_date, location=location, tracks=tracks, text=text)
 
-            for media_id in request.POST.getlist('uploads',[]):
+            for media_id in request.POST.getlist('uploads', []):
                 media = Media.objects.get(pk=int(media_id))
                 media.pin = pin
                 media.save()
 
-            return redirect('/trip/'+str(trip_id)+'?post=last') 
+            return redirect('/trip/' + str(trip_id) + '?post=last')
     else:
         form = forms.NewPostForm()
     return render(request, 'new_post.html', {
-        'trip' : trip ,
+        'trip': trip,
         'form': form
+    })
+
+
+def pin_json_view(request, trip_id):
+    trip = get_object_or_404(Trip, pk=trip_id)
+    pins = []
+    for pin in trip.pin_set.all().order_by("pin_date"):
+        pins.append({
+            "name": pin.name,
+            "blurb": pin.text,
+            "date": str(pin.pin_date.date()),
+            "lat": pin.location.lat,
+            "lon": pin.location.lon,
+            "media": [{"previewUrl": m.preview_url(), "url": m.url()} for m in pin.media_set.all()]
         })
+    response = json.dumps(pins)
+    return HttpResponse(response, mimetype='application/json')
+
 
 @login_required
-def finish_view(request,trip_id):
+def finish_view(request, trip_id):
     trip = get_object_or_404(Trip, pk=trip_id)
     if request.user != trip.user:
-        response = {'success':False}
+        response = {'success': False}
     else:
         trip.active = False
         trip.save()
-        response = {'success':True}
+        response = {'success': True}
     response = json.dumps(response)
     return HttpResponse(response, mimetype='application/json')
 
+
 @login_required
-def reactivate_view(request,trip_id):
+def reactivate_view(request, trip_id):
     trip = get_object_or_404(Trip, pk=trip_id)
     if request.user != trip.user:
-        response = {'success':False}
+        response = {'success': False}
     else:
         trip.active = True
         trip.save()
-        response = {'success':True}
+        response = {'success': True}
     response = json.dumps(response)
     return HttpResponse(response, mimetype='application/json')
 
+
 @login_required
-def delete_trip_view(request,trip_id):
+def delete_trip_view(request, trip_id):
     trip = get_object_or_404(Trip, pk=trip_id)
     if request.user != trip.user:
-        response = {'success':False}
+        response = {'success': False}
     else:
         pins = trip.pin_set.all()
         for p in pins:
             p.delete()
         trip.delete()
-        response = {'success':True}
+        response = {'success': True}
     response = json.dumps(response)
     return HttpResponse(response, mimetype='application/json')
 
+
 @login_required
-def delete_pin_view(request,pin_id):
+def delete_pin_view(request, pin_id):
     pin = get_object_or_404(Pin, pk=pin_id)
     if request.user != pin.trip.user:
-        response = {'success':False}
+        response = {'success': False}
     else:
         pin.delete()
-        response = {'success':True}
+        response = {'success': True}
     response = json.dumps(response)
     return HttpResponse(response, mimetype='application/json')
 
@@ -180,23 +211,25 @@ def file_upload_view(request):
             if content_type == "image":
                 media = Image(media=new_file)
                 media.save()
-                result.append({'id':media.id,
-                            'name':new_file.name,
-                            'url':media.pin_display.url})
+                result.append({'id': media.id,
+                               'name': new_file.name,
+                               'url': media.pin_display.url})
             else:
-                result.append({"error" : new_file.name + " is not a valid image"})
+                result.append(
+                    {"error": new_file.name + " is not a valid image"})
 
         response_data = json.dumps(result)
         return HttpResponse(response_data, mimetype='application/json')
     else:
-        pass #return 404
+        pass  # return 404
+
 
 @login_required
-def edit_profile_view(request,username):
+def edit_profile_view(request, username):
     if not username == request.user.username:
         return redirect("/profile/" + username)
     if request.method == "POST":
-        form = forms.EditProfileForm(request.POST,request.FILES)
+        form = forms.EditProfileForm(request.POST, request.FILES)
         if form.is_valid():
             user = User.objects.get(username=username)
             profile = user.get_profile()
@@ -204,7 +237,7 @@ def edit_profile_view(request,username):
             lat = form.cleaned_data['lat']
             lon = form.cleaned_data['lon']
             if lat and lon:
-                location = Location.objects.create(lat=lat,lon=lon)
+                location = Location.objects.create(lat=lat, lon=lon)
                 profile.hometown = location
 
             if form.cleaned_data['first_name']:
@@ -225,12 +258,11 @@ def edit_profile_view(request,username):
 
             profile.save()
             user.save()
-            
+
             return redirect("/profile/" + username)
     else:
         form = forms.EditProfileForm()
     return render(request, 'edit_profile.html', {
-        'profile_user' : User.objects.get(username=username),
-        'form' : form 
+        'profile_user': User.objects.get(username=username),
+        'form': form
     })
-
