dbddf3c208929632eac81141b1f8e2d59d87ef11
Jennifer Blight <jgblight@gmail.com>
2015-07-17 09:00:44 -0700

    static files

--- intrepid/settings.py
+++ intrepid/settings.py
@@ -95,7 +95,7 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.environ['OPENSHIFT_REPO_DIR']
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
